7be83acb07b2fa3ce2cf90a4ada2da9591c2dcb1
MelissaPerez09 <per21385@uvg.edu.gt>
2022-11-14 08:17:02 -0600

    .cpp created

---
+++ src/calculos.cpp
@@ -0,0 +1,13 @@
+
+
+/*---------------------------------------------------------------------------
+Copyright (C), 2022-2023, bl33h, MelissaPerez09, FabianJuarez182, SebasJuarez
+@author Sara Echeverria, Melissa Perez, Fabian Juarez, Sebastian Juarez
+FileName: Proyecto03
+@version: I
+Creation: 14/11/2022
+Last modification: 14/11/2022
+----------------------------------------------------------------------------------------------------
+* Descripcion: 
+* Procesamiento de datos para realizar calculos desde un un archivo
+-----------------------------------------------------------------*/
